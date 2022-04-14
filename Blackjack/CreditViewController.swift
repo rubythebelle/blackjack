767830c1444d470b4f9e809d7b92f411c80acd96
Ruby Kim <rubybellekim@gmail.com>
2022-04-15 02:01:18 +0900

    edit(2)

--- Blackjack/CreditViewController.swift
+++ Blackjack/CreditViewController.swift
@@ -7,25 +7,8 @@
 
 import UIKit
 
-class CreditViewController: UIViewController, UITableViewDelegate {
+class CreditViewController: UIViewController {
     
-    @IBOutlet weak var houseTableView: UITableView!;
-    @IBOutlet weak var playerTableView: UITableView!;
-    
-    var cards = [] as NSArray;
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        houseTableView.dataSource = self;
-        houseTableView.delegate = self;
-        
-//        playerTableView.dataSource = self;
-        playerTableView.delegate = self;
-        
-        cards = ["Spades", "Hearts", "Clubs", "Diamonds"];
-    }
     
 
     /*
@@ -38,25 +21,6 @@ class CreditViewController: UIViewController, UITableViewDelegate {
     }
     */
     
-    func houseTableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cards.count;
-    }
-    func playerTableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cards.count;
-    }
-    
-    func houseTableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = houseTableView.dequeueReusableCell(withIdentifier: "houseCell", for: indexPath);
-        
-        let cardimg1 = cell.viewWithTag(1) as! UIImageView;
-        
-        cardimg1.image = UIImage(named: "\(cards[indexPath.row]).png")
-        
-        let cardimg2 = cell.viewWithTag(2) as! UIImageView;
-        
-        cardimg2.image = UIImage(named: "\(cards[indexPath.row]).png")
-        
-        return cell;
-    }
+
 
 }
